36bd5833f3ffabfc661d0a0917361ff66886346a
cvija1 <djordje.cvija.cvijanovic@gmail.com>
2023-09-26 12:54:38 +0200

    general set for front

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -62,44 +62,47 @@ const Header = () => {
                     </form>
                   </li>
                   <li className="nav-item mx-2 my-2 my-lg-0">
-                    <a
-                      href="#"
-                      data-bs-toggle="dropdown"
-                      className="nav-link dropdown-toggle p-0"
-                      id="iconToggle"
-                    >
-                      <img
-                        className="rounded-circle  outer-navbar"
-                        src={
-                          user.avatarUrl
-                            ? `http://localhost:5000/${user.avatarUrl}`
-                            : "https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"
-                        }
-                      />
-                      <span className="ms-2">{user.username}</span>
-                    </a>
-                    <ul
-                      className="dropdown-menu dropdown-menu-end dropdown-menu-dark"
-                      aria-labelledby="navbarDropdownMenuLink2"
-                    >
-                      <li>
-                        <Link
-                          className="dropdown-item ms-lg-auto"
-                          to={`/profile/${user._id}`}
-                        >
-                          Profil
-                        </Link>
-                      </li>
-                      <li>
-                        <Link
-                          className="dropdown-item"
-                          onClick={handleLogout}
-                          to={"/"}
-                        >
-                          Odjavi se
-                        </Link>
-                      </li>
-                    </ul>
+                    <div class="dropdown">
+                      <a
+                        href="#"
+                        data-bs-toggle="dropdown"
+                        className="nav-link dropdown-toggle p-0"
+                        id="iconToggle"
+                      >
+                        <img
+                          className="rounded-circle  outer-navbar"
+                          src={
+                            user.avatarUrl
+                              ? `http://localhost:5000/${user.avatarUrl}`
+                              : "https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"
+                          }
+                        />
+
+                        <span className="ms-2">{user.username}</span>
+                      </a>
+                      <ul
+                        className="dropdown-menu dropdown-menu-end dropdown-menu-dark"
+                        aria-labelledby="navbarDropdownMenuLink2"
+                      >
+                        <li>
+                          <Link
+                            className="dropdown-item ms-lg-auto"
+                            to={`/profile/${user._id}`}
+                          >
+                            Profil
+                          </Link>
+                        </li>
+                        <li>
+                          <Link
+                            className="dropdown-item"
+                            onClick={handleLogout}
+                            to={"/"}
+                          >
+                            Odjavi se
+                          </Link>
+                        </li>
+                      </ul>
+                    </div>
                   </li>
                 </>
               ) : (
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,17 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import Spinner from "../components/Spinner";
 import AuthContext from "../context/auth/AuthContext";
 import { useNavigate } from "react-router-dom";
+import { io } from "socket.io-client";
 const Home = () => {
+  const [conversations, setConversations] = useState([]);
+  const [currentChat, setCurrentChat] = useState(null);
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState("");
+  const [arrivalMessage, setArrivalMessage] = useState(null);
+  const [onlineUsers, setOnlineUsers] = useState([]);
+  const socket = useRef();
+  const scrollRef = useRef();
   const { user } = useContext(AuthContext);
   const navigate = useNavigate();
   useEffect(() => {
@@ -10,6 +19,88 @@ const Home = () => {
       navigate(`/login`);
     }
   }, [user]);
+
+  useEffect(() => {
+    socket.current = io("ws://localhost:5000");
+    socket.current.on("getMessage", (data) => {
+      setArrivalMessage({
+        sender: data.senderId,
+        text: data.text,
+        createdAt: Date.now(),
+      });
+    });
+  }, []);
+
+  useEffect(() => {
+    arrivalMessage &&
+      currentChat?.members.includes(arrivalMessage.sender) &&
+      setMessages((prev) => [...prev, arrivalMessage]);
+  }, [arrivalMessage, currentChat]);
+
+  useEffect(() => {
+    socket.current.emit("addUser", user._id);
+    socket.current.on("getUsers", (users) => {
+      setOnlineUsers(
+        user.followings.filter((f) => users.some((u) => u.userId === f))
+      );
+    });
+  }, [user]);
+
+  useEffect(() => {
+    const getConversations = async () => {
+      try {
+        const res = await axios.get("/conversations/" + user._id);
+        setConversations(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getConversations();
+  }, [user._id]);
+
+  useEffect(() => {
+    const getMessages = async () => {
+      try {
+        const res = await axios.get("/messages/" + currentChat?._id);
+        setMessages(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getMessages();
+  }, [currentChat]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const message = {
+      sender: user._id,
+      text: newMessage,
+      conversationId: currentChat._id,
+    };
+
+    const receiverId = currentChat.members.find(
+      (member) => member !== user._id
+    );
+
+    socket.current.emit("sendMessage", {
+      senderId: user._id,
+      receiverId,
+      text: newMessage,
+    });
+
+    try {
+      const res = await axios.post("/messages", message);
+      setMessages([...messages, res.data]);
+      setNewMessage("");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    scrollRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
+
   return (
     <>
       <section className="bg-dark flex-grow-1">
